6b752f922a83cc3ef6fb302c26ac36d4e89bc9bd
rafavlack <rafavlack@gmail.com>
2024-09-02 16:12:12 -0400

    firts commit

---
+++ src/main/java/org/javadev/Main.java
@@ -0,0 +1,9 @@
+package org.javadev;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! todays is: "+ LocalDate.now());
+    }
+}
